37e7384895e51e4bbf3f2d4fb5fef90a7c0d0a38
mercy-tatum <mercy.tatum@gmail.com>
2015-09-17 10:41:17 -0700

    complete mad lib

--- mad_lib.py
+++ mad_lib.py
@@ -1,1 +1,13 @@
-Mad Lib
+name = input ("Please enter a man's name")
+madlib = name
+madlib += " is just a dude "
+madlib += input("Enter an occupation ")
+madlib += " learning how to mix cocktails. Then, one day"
+madlib += input("Enter a noun")
+madlib += " . When {} tried to test out some spanish cocktails he ".format(name)
+madlib += input("Enter a noun")
+madlib += " exploded up to the ceiling, sending fiery cinnamon everywhere"
+madlib += input("")
+madlib += "and {} was never allowed back again".format(name)
+print(madlib)
+
